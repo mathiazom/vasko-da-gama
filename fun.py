27d6f79cc6a1340c69b6f21e2d8c2c39d61b71dd
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-09-30 20:54:49 +0200

    Preserve filename and extension for cat imagery 😽

--- fun.py
+++ fun.py
@@ -9,9 +9,9 @@ from definitions import APP_ROOT
 config = Config.from_config_file(APP_ROOT / "config.yaml")
 
 
-def upload_to_transfersh(url, byte_like) -> Optional[str]:
+def upload_to_transfersh(url: str, filename: str, byte_like) -> Optional[str]:
     # Upload to self-hosted transfer.sh instance, and return upload url
-    res = requests.post(url, files={secrets.token_hex(8): byte_like})
+    res = requests.post(url, files={filename: byte_like})
     if not res.ok:
         print(f"[WARNING] Failed to upload file to transfer.sh instance at {url}")
         return None
@@ -29,7 +29,11 @@ def get_non_existent_cat() -> Optional[str]:
     if not res.ok:
         print("[WARNING] Failed to retrieve non existent cat")
         return None
-    return upload_to_transfersh(config.fun.transfersh_url, res.content)
+    return upload_to_transfersh(
+        config.fun.transfersh_url,
+        f"{secrets.token_hex(4)}.jpg",
+        res.content
+    )
 
 
 def get_real_cat() -> Optional[str]:
@@ -40,14 +44,18 @@ def get_real_cat() -> Optional[str]:
         print("[FAILED] Could not upload real cat, fun config missing.")
         return None
     try:
-        image_url = requests.get(
+        real_cat_res = requests.get(
             url="https://api.thecatapi.com/v1/images/search",
             headers={"x-api-key": config.fun.cat_api_key}
-        ).json()[0]['url']
+        )
     except:
         print("[WARNING] Failed to retrieve real cat")
         return None
+    real_cat = real_cat_res.json()[0]
+    real_cat_url = real_cat['url']
+    real_cat_filename = real_cat_url.split("/")[-1]
     return upload_to_transfersh(
         config.fun.transfersh_url,
-        requests.get(image_url).content
+        real_cat_filename,
+        requests.get(real_cat_url).content
     )
