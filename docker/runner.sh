9e619d799a2b8bf6116d85a665b9fd77d2f09e97
Mathias Oterhals Myklebust <mathias@oterbust.no>
2023-08-22 09:39:13 +0200

    chore: dockerize
    migrated from mathiazom/vasko-da-gama-docker

---
+++ docker/runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+touch /var/log/vasko.log
+
+# Start logging in background
+tail -f /var/log/vasko.log &
+
+echo 'Generating crontab' >> /var/log/vasko.log
+python -u cron_generator.py vasko-cron
+
+echo 'Installing crontab' >> /var/log/vasko.log
+crontab -r
+crontab vasko-cron
+crontab -l >> /var/log/vasko.log
+
+echo 'Starting cron' >> /var/log/vasko.log
+# Start cron daemon and keep container running
+cron && tail -f /dev/null
